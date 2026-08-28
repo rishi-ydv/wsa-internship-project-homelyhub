391f746e0b265663e210c00b20e01490c14c91ec
Rishi-Ydv <ydvrishi1@gmail.com>
2026-08-28 13:08:17 +0545

    feat: create booking schema with references and populate middleware

---
+++ backend/src/models/booking.model.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+  property:{
+    type:mongoose.Schema.ObjectId,
+    ref:"Property",
+    required:[true, "Booking must belong to a Property"],
+  },
+  user:{
+    type:mongoose.Schema.ObjectId,
+    ref:"User",
+    required:[true, "Booking must belong to a user"]
+  },
+  price:{
+    type:Number,
+    required:[true, "Booking must have a price"]
+  },
+  createdAt:{
+    type:Date,
+    default:Date.now()
+  },
+  paid:{
+    type:Boolean,
+    default:true
+  },
+  fromDate:{
+    type:Date,
+  },
+  toDate:{
+    type:Date,
+  },
+  guests:{
+    type:Number,
+  },
+  numberOfnights:{
+    type:Number,
+  },
+},
+{timestamps:true}
+)
+
+bookingSchema.pre(/^find/, function(next){
+  this.populate("user").populate({
+    path:property,
+    select:"maximumGuest images propertyName address"
+  });
+  next();
+});
+
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+export {Booking};
