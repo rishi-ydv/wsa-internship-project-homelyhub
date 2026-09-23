cabbebd512306b239adf361581111bc362987a0c
Rishi-Ydv <ydvrishi1@gmail.com>
2026-09-23 13:12:44 +0545

    Revert "chore: update API base URL to Render backend"
    This reverts commit 6dc0c7aa53e387c818e5d9acac7f95b4a8ec5b54.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'https://homelyhub-internship-b8.onrender.com',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       }
