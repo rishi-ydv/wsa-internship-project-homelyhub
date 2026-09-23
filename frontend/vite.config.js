aa75400d561f9f8933f66493cea1419efa1a6de8
Rishi-Ydv <ydvrishi1@gmail.com>
2026-09-23 13:46:40 +0545

    chore: update Vite proxy to use Render backend in development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy:{
-      '/api':{
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_PROXY_TARGET,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
+});
